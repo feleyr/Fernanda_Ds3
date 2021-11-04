1f51530c9be0d399f977f76543d3e8a9771577e6
alessio <alessiojunin@gmail.com>
2021-11-04 10:59:25 -0300

    Commit da aula de 11/11/21

--- src/main/java/com/alessiojr/demojpa/service/PessoaService.java
+++ src/main/java/com/alessiojr/demojpa/service/PessoaService.java
@@ -1,4 +1,42 @@
 package com.alessiojr.demojpa.service;
 
+import com.alessiojr.demojpa.domain.Pessoa;
+import com.alessiojr.demojpa.repository.PessoaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PessoaService {
+
+    private final Logger log = LoggerFactory.getLogger(PessoaService.class);
+
+    private final PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public List<Pessoa> findAllList(){
+        return pessoaRepository.findAll();
+    }
+
+    public Optional<Pessoa> findOne(Long id) {
+        log.debug("Request to get Pessoa : {}", id);
+        return pessoaRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        log.debug("Request to delete Pessoa : {}", id);
+        pessoaRepository.deleteById(id);
+    }
+
+    public Pessoa save(Pessoa pessoa) {
+        log.debug("Request to save Pessoa : {}", pessoa);
+        pessoa = pessoaRepository.save(pessoa);
+        return pessoa;
+    }
 }
